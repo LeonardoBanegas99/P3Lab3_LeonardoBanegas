e052d622a84a67169d825aed543bf694c810514c
LeonardoBanegas99 <leobanegas99@unitec.edu>
2018-05-11 16:59:37 -0600

    fin

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sstream>
+#include <math.h>
 
 using namespace std;
 
@@ -71,7 +72,53 @@ int main(){
 			break;
                 }
 		case 2:{
-					
+			string poli;	
+			cout  << "Ingrese un polinomio cuadratico de esta forma ax^2+bx+c" << endl;
+			cin >> poli;
+			string a,b,c;
+			a = poli.at(0);
+			b = poli.at(5);
+			c = poli.at(8);
+			int a1,b1,c1;
+			istringstream iss1 (a);
+			iss1 >> a1;
+			istringstream iss2 (b);
+			iss2 >> b1;
+			istringstream iss3 (c);
+			iss3 >> c1;
+			cout << a1 << b1 << c1 << endl;
+			int aelevado = pow(b1,2);
+			int resta = -1*(4*a1*c1);
+			int total = aelevado + resta;
+			if(total < 0){
+				cout << "No hay factores reales" << endl;
+			}else{
+				int raiz = sqrt(total);
+				int arribasuma = (-1*b1)+raiz;
+				int arribaresta = (-1*b1)-raiz;
+				int resp1 = (arribasuma/(2*a1));
+				int resp2 = (arribaresta/(2*a1));
+				cout << "Antes: " << poli << endl;
+				if(resp1 < 0){
+      					cout << "Despues: " << "("<<poli.at(1)<<"+" << (resp1*-1) << ")";
+					if(resp2 < 0){
+						cout << "(" << poli.at(1)<<"+" << resp2*-1 << ")" << endl;
+
+					}else{
+						cout << "(" << poli.at(1) << (resp2*-1) << ")" << endl;
+
+					}
+				}else{
+					cout << "Despues: " << "("<<poli.at(1) << resp1*-1 << "2)";
+					if(resp2 < 0){
+						cout << "(" << poli.at(1)<<"+" << resp2*-1 << ")3" << endl;
+
+					}else{
+						cout << "(" << poli.at(1) << resp2*-1 << ")4" << endl;
+
+				}
+			}
+			}			
 			break;
 		}
 		case 3:{
